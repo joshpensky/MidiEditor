eb2294f46fd4050c821237c9d55b6b210b1c56aa
William Epstein <wepstein712@gmail.com>
2017-06-13 22:35:29 -0400

    Halfwork on will's unnecessary model

--- src/cs3500/music/model/IMusicOperations.java
+++ src/cs3500/music/model/IMusicOperations.java
@@ -3,11 +3,11 @@ package cs3500.music.model;
 import java.util.List;
 
 /**
- * Created by Will on 6/13/2017.
+ * Created by will on 6/13/2017.
  */
 public interface IMusicOperations<T> {
 
-    void addNoe(int startPos, int endPos, int instrument, int pitch, int volume);
+    void addNote(int startPos, int endPos, int instrument, int pitch, int volume);
 
     void removeNote(int startPos, int endPos, int instrument, int pitch, int volume);
 
