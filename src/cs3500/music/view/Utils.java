f34f127d27a685b227513c108e13b0b527725a28
Josh Pensky <joshuapensky@gmail.com>
2017-06-14 17:15:33 -0400

    Moved utils functions to view

--- src/cs3500/music/view/Utils.java
+++ src/cs3500/music/view/Utils.java
@@ -11,14 +11,34 @@ public class Utils {
         if (pitch < 0 || pitch > 127) {
             throw new IllegalArgumentException();
         }
-        return Pitch.values()[pitch % Pitch.values().length];
+        //return Pitch.values()[pitch % Pitch.values().length];
+        int pitches = Pitch.values().length;
+        int middleC = 60;
+        int octave = getOctave(pitch);
+        if (octave >= 4) {
+            middleC += (pitches * (octave - 4));
+
+        } else {
+            middleC -= (pitches * (4 - octave));
+        }
+        System.out.println(middleC + " " + pitch);
+        return Pitch.values()[Math.abs(middleC - pitch)];
     }
 
     public static int getOctave(int pitch) throws IllegalArgumentException {
         if (pitch < 0 || pitch > 127) {
             throw new IllegalArgumentException();
         }
-        return pitch / Pitch.values().length;
+        int pitches = Pitch.values().length;
+        int middleC = 60;
+        if (pitch > middleC) {
+            int up = (int) Math.floor((pitch - middleC) / (double) pitches);
+            return 4 + up;
+        } else {
+            int down = (int) Math.ceil((middleC - pitch) / (double) pitches);
+            return 4 - down;
+        }
+
     }
 
     public static int getDuration(int start, int end) throws IllegalArgumentException {
