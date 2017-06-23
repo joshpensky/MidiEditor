21e4fce638dc27ab6840558643ec93fba09c5cc5
wepstein712 <wepstein712@gmail.com>
2017-06-23 18:46:52 -0400

    Tests for posn

---
+++ test/cs3500/music/view/PosnTest.java
@@ -0,0 +1,43 @@
+package cs3500.music.view;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Created by Will on 6/23/2017.
+ */
+public class PosnTest {
+
+    @Test
+    public void testConstructor() {
+        Posn p = new Posn(12, 12);
+        assertTrue(p != null);
+        assertEquals(p.getX(), 12);
+        assertEquals(p.getY(), 12);
+    }
+
+    @Test
+    public void testGetX() {
+        Posn a = new Posn(0, 0);
+        assertEquals(a.getX(), 0);
+        Posn b = new Posn(20, 10);
+        assertEquals(b.getX(), 20);
+        Posn c = new Posn(-20, -10);
+        assertEquals(c.getX(), -20);
+    }
+
+    @Test
+    public void testGetY() {
+        Posn a = new Posn(0, 0);
+        assertEquals(a.getY(), 0);
+        Posn b = new Posn(20, 10);
+        assertEquals(b.getY(), 10);
+        Posn c = new Posn(-20, -10);
+        assertEquals(c.getY(), -10);
+    }
+
+
+
+}
